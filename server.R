fdc06313a9318353b595761f79305dd3d9240363
Darrell Abrau <darrellabrau@PATH-3SCGTFL-LT.ucsf.edu>
2018-06-04 16:31:11 -0700

    update heat map params

--- server.R
+++ server.R
@@ -27,7 +27,9 @@ server <- function(input, output) {
       heatmap_data,
       colors = colorRampPalette(c("green", "black", "red"))(80),
       margins = c(200, 200),
-      hclust_method = "ward.D2"
+      hclust_method = "ward.D2",
+      grid_gap = 1,
+      branches_lwd = 0.3
     )
     
   })
